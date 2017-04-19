4b673407ca289affb3c4d351f68f5e6abe54697d
Harshil-chaudhary <harshilchaudhary8@gmail.com>
2017-04-19 16:21:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+'''input
+test.mp4
+test2.mp4
+1
+'''
+
+from ffmpy import FFmpeg
+
+raw = raw_input('Enter the name ')
+export = raw_input('Save file as ')
+choice = int(raw_input('Enter  \n 1-change format \n 2-trim \n'))
+if choice==2:
+	starttime = raw_input('Start time (hh:mm:ss)  ')
+	duration = raw_input('enter the duration (hh:mm:ss)  ')
+	argument='-ss: '+starttime+' -t: '+duration+' -async 1 -strict -2'
+	ff = FFmpeg(
+		inputs={raw: None},
+		outputs={export : argument}
+		)
+	#-t: duration(delete it to crop till the end) -ss:starttime
+	ff.cmd
+	ff.run()
+elif choice==1:
+	ff = FFmpeg(
+		inputs={raw: None},
+		outputs={export : None}
+		)
+	#-t: duration(delete it to crop till the end) -ss:starttime
+	ff.cmd
+	ff.run()	
